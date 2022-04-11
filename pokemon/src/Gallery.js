bcba1d75f9818dba74fd88983b9416a28a819ed2
mkcrouther@gmail.com <33792608+crouther@users.noreply.github.com>
2022-04-11 13:41:41 -0400

    Draft Resources and Code

---
+++ pokemon/src/Gallery.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Imgix from "react-imgix";
+
+const images = [
+  "ash",
+  "misty",
+  "brock"
+];
+
+const buildURL = imagePath =>
+`https://raw.githubusercontent.com/crouther/test/main/${imagePath}.jpg`;
+
+export const Gallery = () => (
+  <div className="gallery">
+    {images.map(image => (
+      <Imgix
+        sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw"
+        src={buildURL(image)}
+        key={image}
+        imgixParams={{
+          fit: "",
+          fm: "jpg"
+        }}
+        width="393"
+        height="700"
+      />
+      
+    ))}
+  </div>
+);
