4b119aaebf5851da1dd2c2e8442c503f1d8f2dc3
mkcrouther@gmail.com <33792608+crouther@users.noreply.github.com>
2022-04-14 15:05:52 -0400

    Added fetch and database connectivity

--- pokemon/src/Gallery.js
+++ pokemon/src/Gallery.js
@@ -11,11 +11,29 @@ const images = [
   "sabrina"
 ];
 
+
+function PIMG(jarr){
+  for (let i = 0; i < jarr.length; i++){
+    images.push(jarr[i].images);
+  }
+}
+
+function getIMG(){
+	fetch("http://localhost:8080/myuser",{
+			method: "get",
+	})
+	.then((response) => response.json())
+	.then((jarr) => PIMG(jarr));	
+}
+
+
+
 const buildURL = imagePath =>
 `https://raw.githubusercontent.com/crouther/test/main/${imagePath}.jpg`;
 
 export const Gallery = () => (
   <div className="gallery">
+    {getIMG()}
     {images.map(image => (
       <Imgix
         sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw"
