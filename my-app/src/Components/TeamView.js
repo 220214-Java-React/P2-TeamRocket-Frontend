a2da395b8ea9d88af573feaca2eddf6e86c76779
JustinSmith <steelindian66@gmail.com>
2022-04-18 17:22:51 -0500

    Created State

--- my-app/src/Components/TeamView.js
+++ my-app/src/Components/TeamView.js
@@ -1,33 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
+import { useState } from "react";
 import PokemonTeam from "./PokemonTeam";
 
-function componentDidMount() {
-  fetch("http://localhost:8080/team/1")
-    .then((response) => response.json())
-    .then((data) => pokemonTeam(data));
-}
 
-let team1 = "";
-let team2 = "";
-let team3 = "";
-let team4 = "";
-let team5 = "";
-let team6 = "";
-
-function pokemonTeam(team) {
-  team1 = team.position1;
-  team2 = team.position2;
-  team3 = team.position3;
-  team4 = team.position4;
-  team5 = team.position5;
-  team6 = team.position6;
-  
-}
 
 function TeamView() {
+  const [ pokemonState, setPokemonState] = useState('')
+  const pokemonToState = () => {
+   
+
+    fetch("http://localhost:8080/team/1")
+    .then((response) => response.json())
+    .then((data) => setPokemonState(data));
+    console.log(pokemonState);
+  }
+
+
   return (
     <div id="right-box">
-      <button onClick={() => componentDidMount()}>Console</button>
+      <button onClick={() => pokemonToState()}>Console</button>
 
       {
         <img
